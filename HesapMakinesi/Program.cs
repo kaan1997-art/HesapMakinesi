165fb471c301ec679622f23d81bfe7cfa97cf250
kaan1997-art <77634025+kaan1997-art@users.noreply.github.com>
2021-01-31 16:34:47 +0300

    Add project files.

---
+++ HesapMakinesi/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HesapMakinesi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Hesap Makinesi
+            double sayi1, sayi2;
+            
+
+            Console.WriteLine("*** Dört İşlem***");
+
+            Console.Write("Birinci sayıyı giriniz: ");
+            sayi1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("İkinci sayıyı giriniz: ");
+            sayi2 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("İşlem giriniz(\"+\",\" -\",\" *\",\" /\"): ");
+            string islem = Console.ReadLine();
+            switch (islem)
+            {
+                case "+":
+                    Console.WriteLine("Toplama= " + (sayi1 + sayi2));
+                    break;
+
+                case "-":
+                    Console.WriteLine("Çıkartma= " + (sayi1 - sayi2));
+                    break;
+
+                case "*":
+                    Console.WriteLine("Çarpma= " + (sayi1 * sayi2));
+                    break;
+
+                case "/":
+                    Console.WriteLine("Bölme= " + (sayi1 / sayi2));
+                    break;
+            }
+
+
+            
+
+
+
+
+
+
+
+
+
+
+            Console.ReadLine();
+
+            
+
+            
+
+            #endregion
+        }
+    }
+}
---
+++ test/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double s1, s2, toplami, farki, carpimi, bolumu;
+            Console.Write("Birinci Sayıyı Girin : ");
+
+            s1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("İkinci Sayıyı Girin : ");
+
+            s2 = Convert.ToDouble(Console.ReadLine());
+
+            toplami = s1 + s2;
+
+            farki = s1 - s2;
+
+            carpimi = s1 * s2;
+
+            bolumu = s1 / s2;
+
+            Console.WriteLine("Sayıların Toplamı :", toplami);
+
+            Console.WriteLine("Sayıların Farkı : ", farki);
+
+            Console.WriteLine("Sayıların Çarpımı :",carpimi);
+
+            Console.WriteLine("Sayıların Bölümü :",bolumu);
+
+            Console.ReadKey();
+        }
+    }
+}
